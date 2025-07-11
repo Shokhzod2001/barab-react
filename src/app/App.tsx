1ac3b38e2bb3caf4937a5cc1a0d621b1202d30c1
Shokhzod2001 <shahzodabdumalikov1@gmail.com>
2025-07-12 01:49:54 +0900

    BRR: Initial Setup

---
+++ src/app/App.tsx
@@ -0,0 +1,5 @@
+import "../css/app.css"
+
+export const App = () => {
+  return <div>HOME</div>
+}
---
+++ src/app/store.ts
@@ -0,0 +1,15 @@
+import type { Action, ThunkAction } from "@reduxjs/toolkit"
+import { configureStore } from "@reduxjs/toolkit"
+
+export const store = configureStore({
+  reducer: {},
+})
+
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  RootState,
+  unknown,
+  Action
+>
